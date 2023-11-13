eb7745b086a53fcc8b7f016fa6d62cb7628b86f3
Leopoldo Vodak <lvodak@fu-r4-p7.s19.be>
2023-11-13 18:08:07 +0100

    libftprintf42 added

---
+++ test/PRINTF main.c
@@ -0,0 +1,135 @@
+#include "ft_printf.h"
+#include <limits.h>
+#include <stdio.h>
+
+int	main(void)
+{
+	char			*suc;
+	long			nbr;
+	int				smol;
+	unsigned int	uint;
+	void			*ptr;
+
+	uint = 4294967295;
+	suc = "Success";
+	ptr = (void *)suc;
+	smol = 42;
+	nbr = 7748484204820392;
+	ft_printf("--------------|%%P FLAG|--------------\n");
+	   printf("OG    (empty): %p                     \n", (void *)"");
+	ft_printf("|     (empty): %p                     \n", (void *)"");
+	ft_printf("|        NULL: %p                     \n", (void *)NULL);
+	   printf("OG       NULL: %p                     \n", (void *)NULL);
+	ft_printf("|       LLMAX: %p                     \n", (void *)LLONG_MAX);
+       printf("OG      LLMAX: %p                     \n", (void *)LLONG_MAX);
+	ft_printf("|        LMAX: %p                     \n", (void *)LONG_MAX);
+	   printf("OG       LMAX: %p                     \n", (void *)LONG_MAX);
+	ft_printf("|        LMAX: %p                     \n", (void *)LONG_MAX);
+	   printf("OG       LMAX: %p                     \n", (void *)LONG_MAX);
+	ft_printf("|       ULMAX: %p                     \n", (void *)ULONG_MAX);
+	   printf("OG      ULMAX: %p                     \n", (void *)ULONG_MAX);
+	ft_printf("|      -ULMAX: %p                     \n", (void *)-ULONG_MAX);
+	   printf("OG     -ULMAX: %p                     \n", (void *)-ULONG_MAX);
+	ft_printf("|        LMIN: %p                     \n", (void *)LONG_MIN);
+	   printf("OG       LMIN: %p                     \n", (void *)LONG_MIN);
+	ft_printf("|      UI_MAX: %p                     \n", (void *)UINT_MAX);
+	   printf("OG     UI_MAX: %p                     \n", (void *)UINT_MAX);
+	ft_printf("|       I_MIN: %p                     \n", (void *)INT_MIN);
+	   printf("OG      I_MIN: %p                     \n", (void *)INT_MIN);
+	ft_printf("|        -val: %p                    \n", (void *)-14523);
+	   printf("OG       -val: %p                    \n", (void *)-14523);
+	ft_printf("|          -1: %p                     \n", (void *)-1);
+	   printf("OG         -1: %p                     \n", (void *)-1);
+	ft_printf("|          42: %p                     \n", (void *)42);
+	   printf("OG         42: %p                     \n", (void *)42);
+	ft_printf("--------------|%%d FLAG|--------------\n");
+	ft_printf("|      UI_MAX: %d                     \n", INT_MAX);
+	   printf("OG     UI_MAX: %d                     \n", INT_MAX);
+    ft_printf("|       I_MIN: %d                     \n", INT_MIN);
+	   printf("OG      I_MIN: %d                     \n", INT_MIN);
+	   printf("OG         -1: %d                     \n", -1);
+	ft_printf("|          42: %d                     \n", 42);
+	   printf("OG         42: %d                     \n", 42);
+	ft_printf("|         -42: %d                     \n", -42);
+	   printf("OG        -42: %d                    \n", -42);
+
+	ft_printf("--------------|%%i FLAG|--------------\n");
+	ft_printf("|      UI_MAX: %i                     \n", INT_MAX);
+	   printf("OG     UI_MAX: %i                     \n", INT_MAX);
+	ft_printf("|       I_MIN: %i                     \n", INT_MIN);
+	   printf("OG      I_MIN: %i                     \n", INT_MIN);
+	ft_printf("|        -val: %i                    \n", -14523);
+	   printf("OG       -val: %i                    \n", -14523);
+	ft_printf("|          -1: %i                     \n", -1);
+	   printf("OG         -1: %i                     \n", -1);
+	ft_printf("|          42: %i                     \n", 42);
+	   printf("OG         42: %i                     \n", 42);
+	ft_printf("|         -42: %i                     \n", -42);
+	   printf("OG        -42: %i                     \n", -42);	
+
+	ft_printf("--------------|%%u FLAG|--------------\n");
+	ft_printf("|      UI_MAX: %u                     \n", UINT_MAX);
+	   printf("OG     UI_MAX: %u                     \n", UINT_MAX);
+    ft_printf("|      UI_MAX: %u                     \n", LONG_MAX);
+	   printf("OG     UI_MAX: %u                     \n", LONG_MAX);
+	ft_printf("|      UI_MAX: %u                     \n", 9223372036854775807LL);
+	   printf("OG     UI_MAX: %u                     \n", 9223372036854775807LL);
+	ft_printf("|      UL_MAX: %u                     \n", UINT_MAX);
+	   printf("OG     UL_MAX: %u                     \n", UINT_MAX);
+       printf("|       L_MAX: %u                     \n", UINT_MAX);
+	   printf("OG      L_MAX: %u                     \n", UINT_MAX);
+
+	ft_printf("|           0: %u                     \n", 0);
+	   printf("OG          0: %u                     \n", 0);
+	ft_printf("|          42: %u                     \n", 42);
+	   printf("OG         42: %u                     \n", 42);
+
+	ft_printf("--------------|%%x FLAG|--------------\n");
+	
+	ft_printf("|       UIMAX: %x                     \n",  2019496700);
+	   printf("OG      UIMAX: %x                     \n",  2019496700);
+	ft_printf("|       ULMAX: %x                     \n", ULONG_MAX);
+	   printf("OG      ULMAX: %x                     \n", ULONG_MAX);
+	ft_printf("|    LONG_MIN: %x                     \n", LONG_MIN);
+	   printf("OG   LONG_MIN: %x                     \n", LONG_MIN);
+	ft_printf("|    LONG_MAX: %x                     \n", LONG_MAX);
+	   printf("OG   LONG_MAX: %x                     \n", LONG_MAX);
+	ft_printf("|        -val: %x                    \n", -14523);
+	   printf("OG       -val: %x                    \n", -14523);
+	ft_printf("|          -1: %x                     \n", -1);
+	   printf("OG         -1: %x                     \n", -1);
+	ft_printf("|          42: %x                     \n", 42);
+	   printf("OG         42: %x                     \n", 42);
+	ft_printf("|         -42: %x                     \n", -42);
+	   printf("OG        -42: %x                     \n", -42);
+    	ft_printf("--------------|%%X FLAG|--------------\n");
+	
+	ft_printf("|        LMAX: %X                     \n", UINT_MAX);
+	   printf("OG       LMAX: %X                     \n", UINT_MAX);
+	ft_printf("|        -val: %X                    \n", -14523);
+	   printf("OG       -val: %X                    \n", -14523);
+	ft_printf("|           1: %X                     \n",1);
+	   printf("OG          1: %X                     \n", 1);
+	ft_printf("|          42: %X                     \n", 42);
+	   printf("OG         42: %X                     \n", 42);
+	ft_printf("|          42: %X                     \n", 42);
+	   printf("OG         42: %X                     \n", 42);
+
+	// ft_printf("%");
+	//   printf("%");
+
+	//printf("%s\n", "k l&NXtF`}GJG,$EFOH$][w\n 37kbNJyv\v-cp=0aJ.ncZ0z541ZCd/ JqXh&l>9.,l7iMv Rbwn:#[>*x5<\trU");
+	//ft_printf("coucou %s coucou \n", "k l&NXtF`}GJG,$EFOH$][w\n 37kbNJyv\v-cp=0aJ.ncZ0z541ZCd/ JqXh&l>9.,l7iMv Rbwn:#[>*x5<\trU");
+	// int xz = ft_printf("?a^X%c/7}(}fV77%sPVT\r3RbRw%cqUsICC%XKX4j&U%s{D?%%/$M", 0, "k l&NXtF`}GJG,$EFOH$][w\n 37kbNJyv\v-cp=0aJ.ncZ0z541ZCd/ JqXh&l>9.,l7iMv Rbwn:#[>*x5<\trU", -1544082211, 48713326, "nE*Iz0.}yZvi*hV%\r8c\t KA_LxEM2kOaT$|{arV|Kfp) s8xhX1riEXrm{CU\f?\"\vv\rH0%2U60Nq^t]e");
+	// printf("\n------------\n");
+	// int xy =    printf("?a^X%c/7}(}fV77%sPVT\r3RbRw%cqUsICC%XKX4j&U%s{D?%%/$M", 0, "k l&NXtF`}GJG,$EFOH$][w\n 37kbNJyv\v-cp=0aJ.ncZ0z541ZCd/ JqXh&l>9.,l7iMv Rbwn:#[>*x5<\trU", -1544082211, 48713326, "nE*Iz0.}yZvi*hV%\r8c\t KA_LxEM2kOaT$|{arV|Kfp) s8xhX1riEXrm{CU\f?\"\vv\rH0%2U60Nq^t]e");
+	// int xz = ft_printf("%%%w%%%r%%%y%%%k%%%%%%z%%%%%%%%%%h%%%%%k%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n");
+	// int xy = printf("%%%w%%%r%%%y%%%k%%%%%%z%%%%%%%%%%h%%%%%k%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n");
+	//ft_printf ("M = %c %c %c aaaaaaaaaaaaaa c%s %i s\n", '1', 0, '0', "coucou          wkuycfgliecf", 548);
+	//   printf ("O = %c %c %c aaaaaaaaaaaaaa c%s %i\n", '1', 0, '0', "coucou          wkuycfgliecf", 548);
+	//int a = ft_printf("%%|%c|%%|%s|%%|%d|%%|%i|%%|%u|%%|%x|%%|%X|%%|%%| %%|%c|%%|%s|%%|%d|%%|%i|%%%u%%%x%%%X%%%% %%%c%%%s%%%d%%%i%%%u%%%x%%%X%%%% %c%%", 'A', "42", 42, 42 ,42 , 42, 42, 'B', "-42", -42, -42 ,-42 ,-42, 42, 'C', "0", 0, 0 ,0 ,0, 42, 0);
+	//ft_printf("\n");
+	//int b =    printf("%%|%c|%%|%s|%%|%d|%%|%i|%%|%u|%%|%x|%%|%X|%%|%%| %%|%c|%%|%s|%%|%d|%%|%i|%%%u%%%x%%%X%%%% %%%c%%%s%%%d%%%i%%%u%%%x%%%X%%%% %c%%", 'A', "42", 42, 42 ,42 , 42, 42, 'B', "-42", -42, -42 ,-42 ,-42, 42, 'C', "0", 0, 0 ,0 ,0, 42, 0);
+	//printf ("o %i = m %i\n", b, a);
+	//return (0);
+}
---
+++ test/test printf.c
@@ -0,0 +1,29 @@
+// int main()
+// {
+// 	// ft_printf("%%%c", 'a'); ft_putstr_fd("\n", 1);
+// 	// ft_printf("%ceci est un char %c", 'c', 'a'); ft_putstr_fd("\n", 1);
+// 	// ft_printf(" %c %c %c ", '0', 0, '1'); ft_putstr_fd("\n", 1);
+// 	// ft_printf(" %c %c %c ", '2', '1', 0); ft_putstr_fd("\n", 1);
+// 	// ft_printf(" NULL %s NULL ", NULL); ft_putstr_fd("\n", 1);
+// 	// printf("%%%c\n", 'a');
+// 	// printf("%ceci est un char %c\n", 'c', 'a');
+// 	// printf(" %c %c %c \n", '0', 0, '1');
+// 	// printf(" %c %c %c \n", '2', '1', 0);
+// 	// printf(" NULL %s NULL \n", NULL);
+// 	// ft_printf(" %p %p ", ULONG_MAX, -ULONG_MAX); ft_putstr_fd("\n", 1);
+// 	// ft_printf(" %p %p ", 0, 0); ft_putstr_fd("\n", 1);
+// 	// ft_printf(" %u ", -1); ft_putstr_fd("\n", 1);
+// 	// ft_printf(" %u ", 10); ft_putstr_fd("\n", 1);
+// 	// ft_printf(" %u ", 11); ft_putstr_fd("\n", 1);
+// 	// printf(" %p %p \n", ULONG_MAX, -ULONG_MAX);
+// 	// printf(" %p %p \n", 0, 0);
+// 	// printf(" %u \n", -1);
+// 	// printf(" %u \n", 10);
+// 	// printf(" %u \n", 11);
+// 	//ft_printf("%%%c%%%s%%%d%%%i%%%u%%%x%%%X%%%% %%%c%%%s%%%d%%%i%%%u%%%x%%%X%%%% %%%c%%%s%%%d%%%i%%%u%%%x%%%X%%%% %c%%", 'A', "42", 42, 42 ,42 , 42, 42, 'B', "-42", -42, -42 ,-42 ,-42, 42, 'C', "0", 0, 0 ,0 ,0, 42, 0); //ft_putstr_fd("\n", 1);
+// 	//printf("%%%c%%%s%%%d%%%i%%%u%%%x%%%X%%%% %%%c%%%s%%%d%%%i%%%u%%%x%%%X%%%% %%%c%%%s%%%d%%%i%%%u%%%x%%%X%%%% %c%%", 'A', "42", 42, 42 ,42 , 42, 42, 'B', "-42", -42, -42 ,-42 ,-42, 42, 'C', "0", 0, 0 ,0 ,0, 42, 0); //ft_putstr_fd("\n", 1);
+// 	//ft_printf("%i  \n", INT32_MAX); //ft_putstr_fd("\n", 1);
+// 	// ft_printf("%u  \n", 12594); //ft_putstr_fd("\n", 1);
+// 	ft_printf("kmk.`z%xG^%cDC%dKEI{>\f4~+P%dpQHFeYv(\t %X_p&)%i?ztjC :%xW ?)\n", -1020725365, -1031747592, -888783013, -45416677, -651163268, -1553551862, -107692847);
+// 	printf("kmk.`z%xG^%cDC%dKEI{>\f4~+P%dpQHFeYv(\t %X_p&)%i?ztjC :%xW ?)\n", -1020725365, -1031747592, -888783013, -45416677, -651163268, -1553551862, -107692847);
+// }
